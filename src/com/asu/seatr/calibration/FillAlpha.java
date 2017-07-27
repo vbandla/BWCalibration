3393870cb2219a5b315f71ec7c3b1f77c3acc283
vbandla <vbandla@asu.edu>
2017-07-26 23:27:11 -0700

    Fill Alpha java file

--- src/com/asu/seatr/calibration/FillAlpha.java
+++ src/com/asu/seatr/calibration/FillAlpha.java
@@ -1,5 +1,11 @@
 package com.asu.seatr.calibration;
 
+import java.util.ArrayList;
+
+import com.asu.seatr.utils.GlobalConstants;
+import com.asu.seatr.utils.Operations;
+import com.asu.seatr.utils.Utils;
+import com.asu.seatr.helpers.AlphaHelperFunction;
 
 /**
  * @author Venkata Krishna Bandla created on July 25, 2017
@@ -7,5 +13,46 @@ package com.asu.seatr.calibration;
  */
 public class FillAlpha {
 
-	
+	public static void fillAlpha(){
+		
+		int NS = GlobalConstants.total_Students;
+		int NK = GlobalConstants.total_KCs;
+		
+		for(int St = 0 ; St < NS ; St++){
+			int S = Utils.getStudent(St);
+		
+			for(int K = 0; K < NK; K++){
+				int innerKc = Utils.getKc(K);
+				
+				Utils.updateFetchAlpha2(S, innerKc, 1, 1, Utils.getInitialMasteryMap(innerKc));
+				Utils.updateFetchAlpha2(S, innerKc, 1, 0, Operations.substractDouble((double) 1, Utils.getInitialMasteryMap(innerKc)));
+				
+				Utils.updateFetchAlpha(S, innerKc, 0, 1, Utils.getInitialMasteryMap(innerKc));
+				Utils.updateFetchAlpha(S, innerKc, 0, 0, Operations.substractDouble((double) 1, Utils.getInitialMasteryMap(innerKc)));
+			}
+			
+			for (int T = 1; T <= Utils.getLast(S); T++) {
+				
+				int question = Utils.getQuestion(S, T);
+				ArrayList<Integer> KCs = Utils.getQuestionMatrix(question);
+				for(int K = 0; K < NK; K++){
+					
+					int innerKc = Utils.getKc(K);
+					if (KCs.contains(innerKc)) {
+						
+						AlphaHelperFunction.Alpha(S,innerKc,T,1);
+						AlphaHelperFunction.Alpha(S,innerKc,T,0);
+						
+					}
+					
+					else{
+						
+						Utils.updateFetchAlpha(S, innerKc, T, 1, Utils.getFetchAlpha(S, innerKc, T-1, 1));
+						Utils.updateFetchAlpha(S, innerKc, T, 0, Utils.getFetchAlpha(S, innerKc, T-1, 0));
+						
+					}
+				}
+			}	
+		}
+	}
 }
