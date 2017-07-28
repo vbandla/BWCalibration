3939d1325ef2efe5fc5797fafd1b2f05db350931
vbandla <vbandla@asu.edu>
2017-07-27 17:15:40 -0700

    EstimateInitialMastery java file

--- src/com/asu/seatr/calibration/EstimateInitialMastery.java
+++ src/com/asu/seatr/calibration/EstimateInitialMastery.java
@@ -1,10 +1,32 @@
 package com.asu.seatr.calibration;
 
+import com.asu.seatr.utils.GlobalConstants;
+import com.asu.seatr.utils.Operations;
+import com.asu.seatr.utils.Utils;
 
 /**
  * @author Venkata Krishna Bandla created on July 27, 2017
  *
  */
 public class EstimateInitialMastery {
+	
+	public static void estimateIntialMastery(){
+		
+		int NK = GlobalConstants.total_KCs;
+		int NS = GlobalConstants.total_Students;
+		
+		for (int K = 0; K < NK; K++) {
+			int Kc = Utils.getKc(K);
+			
+			Double Numerator = (double)0;
+			Double Denominator = (double)0;
+			for (int St = 0; St < NS; St++) {
+				int S = Utils.getStudent(St);
+				
+				Numerator = Operations.addDouble(Numerator, Utils.getFetchBest(S, Kc, 1));
+				Denominator = Operations.addDouble(Denominator, (double)1);
+			}	
+		}
+	}
 
 }
